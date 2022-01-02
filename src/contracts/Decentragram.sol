fff01db27079999d4c7c2b121a85a23e4b3702fa
promehedi <admin@promehedi.com>
2022-01-03 02:10:02 +0600

    TipImages Function

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -18,6 +18,7 @@ contract Decentragram {
 
   // Events
   event ImageCreated(uint indexed id, string hash, string description, uint tipAmount, address payable author);
+  event ImageTipped(uint indexed id, string hash, string description, uint tipAmount, address payable author);
 
   // Upload Images
   function uploadImage(string memory _hash, string memory _description) public {
@@ -30,5 +31,16 @@ contract Decentragram {
 
     emit ImageCreated(imageCount, _hash, _description, 0, payable(msg.sender));
   }
-  
+
+  // Tip Images
+  function tipImageOwner(uint _id) public payable {
+    require(_id > 0 && _id <= imageCount);
+
+    Image memory _image = images[_id];
+    _image.author.transfer(msg.value);
+    _image.tipAmount += msg.value;
+    images[_id] = _image;
+
+    emit ImageTipped(_id, _image.hash, _image.description, _image.tipAmount, _image.author);
+  }  
 }
