5a783f86157c71079631a9e2ec2e8677086a5e97
promehedi <admin@promehedi.com>
2022-01-03 02:41:20 +0600

    Connect with MetaMask

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,53 @@
-import React, { Component } from 'react';
-import Web3 from 'web3';
-import Identicon from 'identicon.js';
-import './App.css';
+import React, { Component } from 'react'
+import Web3 from 'web3'
+import Identicon from 'identicon.js'
+import './App.css'
 import Decentragram from '../abis/Decentragram.json'
 import Navbar from './Navbar'
 import Main from './Main'
 
+const App = () => {
+  const [account, setAccount] = React.useState('')
+  const [loading, setLoading] = React.useState(false)
 
-class App extends Component {
+  React.useEffect(() => {
+    loadWeb3()
+    loadAccount()
+  }, [])
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      account: '',
+  const loadWeb3 = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    } else {
+      window.alert(
+        'Non-Ethereum browser detected. You should consider trying MetaMask!'
+      )
     }
   }
 
-  render() {
-    return (
-      <div>
-        <Navbar account={this.state.account} />
-        { this.state.loading
-          ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
-          : <Main
-            // Code...
-            />
-          }
-        }
-      </div>
-    );
+  const loadAccount = async () => {
+    const accounts = await window.web3.eth.getAccounts()
+    console.log(accounts)
+    setAccount(accounts[0])
   }
+
+  return (
+    <div>
+      <Navbar account={account} />
+      {loading ? (
+        <div id='loader' className='text-center mt-5'>
+          <p>Loading...</p>
+        </div>
+      ) : (
+        <Main
+        // Code...
+        />
+      )}
+    </div>
+  )
 }
 
-export default App;
+export default App
