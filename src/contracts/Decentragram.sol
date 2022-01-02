35a6865e973a7c1616e5ebfe945b6ede80b325ad
promehedi <admin@promehedi.com>
2022-01-03 01:39:58 +0600

    uploadImage Function

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -2,5 +2,24 @@
 pragma solidity ^0.8.0;
 
 contract Decentragram {
+  // Variables
   string public name = "Decentragram";
+  mapping(uint => Image) public images;
+  uint public imageCount = 0;
+
+  // Structs
+  struct Image {
+    uint id;
+    string hash;
+    string description;
+    uint tipAmount;
+    address payable author;
+  }
+
+  // Upload Images
+  function uploadImage(string memory _hash, string memory _description) public {
+    imageCount++;
+    images[imageCount] = Image(imageCount, _hash, _description, 0, payable(msg.sender));
+  }
+  
 }
