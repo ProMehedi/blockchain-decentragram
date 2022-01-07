0f42181f963bb52cee6999f1a89f3fc651f11ec6
promehedi <mmhasan563@gmail.com>
2022-01-07 23:47:58 +0600

    set ropsten and redeploy

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,7 @@
 require('babel-register')
 require('babel-polyfill')
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey')
+const privateKeys = process.env.PRIVATE_KEYS || ''
 
 module.exports = {
   networks: {
@@ -7,6 +9,17 @@ module.exports = {
       host: '127.0.0.1',
       port: 7545,
       network_id: '*', // Match any network id
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          privateKeys.split(','), // Array of account private keys
+          `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}` // Url to an Ethereum Node
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 3,
     },
   },
   contracts_directory: './src/contracts/',
