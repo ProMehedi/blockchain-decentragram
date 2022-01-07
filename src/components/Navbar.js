a9237466c09522dc5cea0fd031f8c48b93244d6a
promehedi <mmhasan563@gmail.com>
2022-01-08 00:18:32 +0600

    Add Container

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,42 +5,44 @@ import photo from '../photo.png'
 const Navbar = ({ account }) => {
   return (
     <nav className='navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow'>
-      <a
-        className='navbar-brand col-sm-3 col-md-2 mr-0'
-        href='http://www.dappuniversity.com/bootcamp'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <img
-          src={photo}
-          width='30'
-          height='30'
-          className='d-inline-block align-top'
-          alt=''
-        />
-        Decentragram
-      </a>
-      <ul className='navbar-nav px-3'>
-        <li className='nav-item text-nowrap d-none d-sm-none d-sm-block'>
-          <small className='text-secondary'>
-            <small id='account'>{account || '0x0'}</small>
-          </small>
-          {account ? (
-            <img
-              className='ml-2'
-              width='30'
-              height='30'
-              src={`data:image/png;base64,${new Identicon(
-                account,
-                30
-              ).toString()}`}
-              alt=''
-            />
-          ) : (
-            <span></span>
-          )}
-        </li>
-      </ul>
+      <div className='container'>
+        <a
+          className='navbar-brand col-sm-3 col-md-2 mr-0'
+          href='http://www.dappuniversity.com/bootcamp'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <img
+            src={photo}
+            width='30'
+            height='30'
+            className='d-inline-block align-top'
+            alt=''
+          />
+          Decentragram
+        </a>
+        <ul className='navbar-nav px-3'>
+          <li className='nav-item text-nowrap d-none d-sm-none d-sm-block'>
+            <small className='text-white'>
+              <small id='account'>{account || '0x0'}</small>
+            </small>
+            {account ? (
+              <img
+                className='ml-2'
+                width='30'
+                height='30'
+                src={`data:image/png;base64,${new Identicon(
+                  account,
+                  30
+                ).toString()}`}
+                alt=''
+              />
+            ) : (
+              <span></span>
+            )}
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
